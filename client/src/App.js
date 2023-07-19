7c563b9ce03adfded04f964c8086731c32ee7130
Krist <kitog@bk.ru>
2023-07-19 13:27:42 +0400

    We got the data from Facebook

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,13 @@ import './App.css'
 import OAuth2Login from 'react-simple-oauth2-login'
 
 function App() {
-  const onSuccess = (res) => {
-    console.log(res)
+  const onSuccess = async (res) => {
+    const accessToken = res.access_token
+    const result = await fetch(
+      `https://graph.facebook.com/me?fields=id,name,picture&access_token=${accessToken}`
+    )
+    const profile = await result.json()
+    console.log(profile)
   }
 
   const onFailure = (res) => {
@@ -18,6 +23,7 @@ function App() {
         responseType="token"
         clientId="139603582496424"
         redirectUri="http://localhost:3000"
+        scope="public_profile"
         onSuccess={onSuccess}
         onFailure={onFailure}
       />
